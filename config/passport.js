e3f52ceba2e191bd2cf4c18a389cf429d555a6b8
naaa <na0908nna@gmail.com>
2024-02-25 11:37:13 +0300

    some

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: (process.env.NODE_ENV === 'production') ? 'https://ararsobusiness.onrender.com/auth/google/callback' : '/auth/google/callback',
+        callbackURL: (process.env.NODE_ENV === 'production') ? 'https://waajjiragalii.onrender.com/auth/google/callback' : '/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
